0630574ca5c7322393d231a38f8501ef9043bb78
Keshav Bainsla <keshavbainsla3435@gmail.com>
2024-07-20 14:23:06 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-ML-Project1"
+AUTHOR_USER_NAME = "KeshavBainsla"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "keshavbainsla3435@gmail.com"
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for ML Project 1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")            
+)              
